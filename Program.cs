131eb288e92097f2c0f777b5225ce9c80265db5f
Slava <d-color.38@mail.ru>
2022-09-09 03:14:48 +0800

    dz23

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Таблица кубов чисел от 1 до N");
+Console.Write("Введите число N: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 1;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 1;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
